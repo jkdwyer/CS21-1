3662eeffbce84011c5fc830abaaa8957b42d7936
Jan Dwyer <jand@cruzio.com>
2017-07-26 17:03:53 -0700

    first rev of LLNode

---
+++ src/LLNode.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.text.*;
+/**
+ * Class LLNode for LinkedList assignment.
+ *
+ * question:  if I import util.*, do I need util.Scanner?
+ *
+ * @author Jan Dwyer
+ * @version 1.0	07/26/2017 
+ *
+ */
+public class LLNode {
+	/**
+	  * Main method (for now.)
+	  * Needs a constructor, methods and member vars.
+	  *
+	  *
+	  */
+	public static void main(String[] args){
+		System.out.println("in LLNode.main");
+	}	// end main.
+}		// end class LLNode.
